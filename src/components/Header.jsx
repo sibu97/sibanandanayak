52e33b150e098574913520d5fa5ded3478d2b012
Sibananda Nayak <nayaksibananda22@gmail.com>
2023-05-31 18:39:54 +0530

    Initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import Logo from '../assets/PASSPHOTO1.ico'
+import Logo from '../assets/logo.ico'
 
 function Header() {
   return (
       <div>
-          <ul>
-              <li><img  alt='logo'><Logo/></img></li>
-              <li><h1 color='white'>Sibananda Nayak</h1></li>
+          <ul id='nav'style={{display:'flex'}}>
+             <li> <img style={{ width: 40, height: 40, borderRadius:'10px'}}
+                  src={Logo} alt='brand'  />
+              </li>
+         
+              <li id='brnd-name'> <h1 style={{
+                  color: 'white', fontFamily:'serif', 
+                  fontSize: '22px', marginTop: '5px', padding:'7px'}}>
+                  SIBANANDA &nbsp;NAYAK</h1>
+              </li>
           </ul>
-      </div>
+      </div> 
   )
 }
 
